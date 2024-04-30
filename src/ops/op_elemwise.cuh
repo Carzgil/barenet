eb300d16e0ee3a1b5f00c7365b2c3bac3418ab9b
carzgil <cars.gilliland@gmail.com>
2024-04-30 13:19:16 -0400

    adding op class for compilation purposes

--- src/ops/op_elemwise.cuh
+++ src/ops/op_elemwise.cuh
@@ -374,6 +374,21 @@ void op_multiply(const Tensor<T> &a, const Tensor<T> &b, Tensor<T> &out)
     }
 }
 
+template<typename T>
+class Op {
+public:
+    std::function<void()> backward_op;
+
+    // Constructor to accept a lambda function for backpropagation
+    Op(std::function<void()> func) : backward_op(func) {}
+
+    // Function to trigger backward operation
+    void backward() {
+        backward_op();
+    }
+};
+
+
 //This operator performs element-wise multiplication of "a" and constant b
 //stores the result in tensor "out"
 template <typename T>
