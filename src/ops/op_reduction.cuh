936204a95e5f048e18da820cb8f132b1237e92cc
cisc0f <francesco@coacci.it>
2024-05-14 17:29:06 -0400

    u

--- src/ops/op_reduction.cuh
+++ src/ops/op_reduction.cuh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "utils/tensor.cuh"
+
 #define REDUCTION_BLOCK_DIM 32
 
 template <typename T>
@@ -11,7 +12,8 @@ public:
     //If x is bigger than accum, stores x in accum and stores x's index (ind_x) to ind_accum
     __host__ __device__ void operator()(const T &x, const int &ind_x, T &accum, int &ind_accum)
     {
-      if (x > accum) {
+        //Lab-1: add your code here
+        if (x > accum) {
             accum = x;
             ind_accum = ind_x;
         }
@@ -26,6 +28,7 @@ public:
     //The accumu's value is updated (to add x).  The ind_x and ind_accum arguments are not used. 
     __host__ __device__ void operator()(const T &x, const int &ind_x, T &accum, int &ind_accum)
     {
+        //Lab-1: add your code here
         accum += x;
     }
 };
@@ -35,25 +38,22 @@ public:
 template <typename OpFunc, typename T>
 __global__ void op_reduction_kernel_colwise(OpFunc f, Tensor<T> in, Tensor<T> out, Tensor<int> out_index, bool get_index)
 {
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    //Lab-1: add your code here
+    int col = threadIdx.x + blockIdx.x * blockDim.x;
 
-	if (col < in.h)
-	{
-		T accum = Index(in, col, 0);
-		int ind_accum = 0;
-		for (int row = 1; row < in.w; ++row)
-		{
-			f(Index(in, col, row), row, accum, ind_accum);
-		}
-		if (!get_index)
-		{
-			Index(out, col, 0) = accum;
-		}
-		else
-		{
-			Index(out_index, col, 0) = ind_accum;
-		}
-	}
+    if (col < in.h) {
+        T accum = Index(in, col, 0);
+        int index = 0;
+
+        for (int row = 1; row < in.w; ++row) {
+            f(Index(in, col, row), row, accum, index);
+        }
+
+        if (!get_index)
+            Index(out, col, 0) = accum;
+        else
+            Index(out_index, col, 0) = index;
+    }
 }
 
 //This kernel function performs row-wise reduction of the "in" tensor and stores the result in "out" tensor.
@@ -61,49 +61,46 @@ __global__ void op_reduction_kernel_colwise(OpFunc f, Tensor<T> in, Tensor<T> ou
 template <typename OpFunc, typename T>
 __global__ void op_reduction_kernel_rowwise(OpFunc f, Tensor<T> in, Tensor<T> out, Tensor<int> out_index, bool get_index)
 {
-    int row = blockIdx.x * blockDim.x + threadIdx.x;
+    //Lab-1: add your code here
+    int row = threadIdx.x + blockIdx.x * blockDim.x;
+    
+   if (row < in.w) {
+        T accum = in.rawp[row*in.stride_w];
+        int index = 0;
 
-	if (row < in.w)
-	{
-		T accum = in.rawp[row * in.stride_w];
-		int ind_accum = 0;
+        for (int col = 1; col < in.h; ++col) {
+            f(in.rawp[row*in.stride_w + col * in.stride_h], col, accum, index);
+        }
 
-		for (int col = 1; col < in.h; ++col)
-		{
-			f(in.rawp[row * in.stride_w + col * in.stride_h], col, accum, ind_accum);
-		}
-		if (!get_index)
-		{
-			out.rawp[row * out.stride_w] = accum;
-		}
-		else
-		{
-			out_index.rawp[row * out_index.stride_w] = ind_accum;
-		}
-	}
+        if (!get_index)
+            out.rawp[row * out.stride_w] = accum;
+        else
+            out_index.rawp[row*out_index.stride_w] = index;
+    }
 }    
 
 template <typename OpFunc, typename T>
 void op_reduction_gpu(OpFunc f, const Tensor<T> &in, Tensor<T> &out, Tensor<int> &out_index, bool get_index = false)
 {
     int out_h = out.h;
-	if (!get_index)
-	{
-		assert((out.h == 1 && in.w == out.w) || (out.w == 1 && in.h == out.h));
-	}
-	else
-	{
-		out_h = out_index.h;
-		assert((out_index.h == 1 && in.w == out_index.w) || (out_index.w == 1 && in.h == out_index.h));
-	}
-	if (in.h > out_h)
-	{
-		op_reduction_kernel_rowwise<<<(in.w + REDUCTION_BLOCK_DIM - 1) / REDUCTION_BLOCK_DIM, REDUCTION_BLOCK_DIM >>>(f, in, out, out_index, get_index);
-	}
-	else
-	{
-		op_reduction_kernel_colwise<<<(in.h + REDUCTION_BLOCK_DIM - 1) / REDUCTION_BLOCK_DIM, REDUCTION_BLOCK_DIM>>>(f, in, out, out_index, get_index);
-	}
+    if (!get_index) {
+        assert((out.h == 1 && in.w == out.w) || (out.w == 1 && in.h == out.h));
+    } else {
+        out_h = out_index.h;
+        assert((out_index.h == 1 && in.w == out_index.w) || (out_index.w == 1 && in.h == out_index.h));
+    }
+    if (in.h > out_h)
+    {
+        //Lab-1: add your code here to launch op_reduction_kernel_rowwise
+        //delete assert(0) when you are finished
+        op_reduction_kernel_rowwise<<<(in.w + REDUCTION_BLOCK_DIM - 1) / REDUCTION_BLOCK_DIM, REDUCTION_BLOCK_DIM>>>(f, in, out, out_index, get_index);
+    }
+    else
+    {
+        //Lab-1: add your code here to launch op_reduction_kernel_colwise
+        //delete assert(0) when you are finished
+        op_reduction_kernel_colwise<<<(in.h + REDUCTION_BLOCK_DIM - 1) / REDUCTION_BLOCK_DIM, REDUCTION_BLOCK_DIM>>>(f, in, out, out_index, get_index);
+    }
 }
 
 
