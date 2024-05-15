8dc01fc182b3b04d6c818c94a8c8747c5e3f212f
cisc0f <francesco@coacci.it>
2024-05-15 15:42:37 -0400

    Test

--- src/ops/op_elemwise.cuh
+++ src/ops/op_elemwise.cuh
@@ -252,13 +252,13 @@ void op_relu(const Tensor<T> &input, Tensor<T> &output) {
     if (input.on_device && output.on_device) {
         op_elemwise_unary_gpu(f, input, output);
 
-        auto backward_op = [&input, &output]() {
-            if (input.grad) { 
-                op_relu_back(input, *output.grad, *input.grad);
-            }
-        };
+        // auto backward_op = [&input, &output]() {
+        //     if (input.grad) { 
+        //         op_relu_back(input, *output.grad, *input.grad);
+        //     }
+        // };
 
-        output.op = std::make_shared<Op<T>>(backward_op);
+        // output.op = std::make_shared<Op<T>>(backward_op);
     } else {
         assert(0); 
     }
@@ -275,11 +275,11 @@ void op_relu_back(const Tensor<T> &in, const Tensor<T> &d_out, Tensor<T> &d_in)
     ReluBackFunc<T> f;
 
     // Ensure the gradient tensor for d_in is properly initialized
-    if (!d_in.grad) {
-        d_in.grad = std::make_shared<Tensor<T>>(in.h, in.w, in.on_device);
-        // Initialize gradient to zero
-        op_const_init(*d_in.grad, 0.0f);
-    }
+    // if (!d_in.grad) {
+    //     d_in.grad = std::make_shared<Tensor<T>>(in.h, in.w, in.on_device);
+    //     // Initialize gradient to zero
+    //     op_const_init(*d_in.grad, 0.0f);
+    // }
 
     if (d_in.on_device && in.on_device && d_out.on_device) {
         op_elemwise_binary_w_bcast_gpu(f, in, d_out, *d_in.grad);
@@ -316,15 +316,15 @@ void op_add(const Tensor<T> &a, const Tensor<T> &b, Tensor<T> &out)
     if (a.on_device && b.on_device && out.on_device) {
         op_elemwise_binary_w_bcast_gpu(f, a, b, out);
 
-        auto backward_op = [&]() {
-            if (a.grad) {
-                op_add(*a.grad, *out.grad, *a.grad);
-            }
-            if (b.grad) {
-                op_add(*b.grad, *out.grad, *b.grad);
-            }
-        };
-        out.op = std::make_shared<Op<T>>(backward_op);
+        // auto backward_op = [&]() {
+        //     if (a.grad) {
+        //         op_add(*a.grad, *out.grad, *a.grad);
+        //     }
+        //     if (b.grad) {
+        //         op_add(*b.grad, *out.grad, *b.grad);
+        //     }
+        // };
+        // out.op = std::make_shared<Op<T>>(backward_op);
     } else {
         assert(0); 
     }
@@ -341,12 +341,12 @@ void op_add(const Tensor<T> &a, T b, Tensor<T> &out)
     if (a.on_device && out.on_device) {
         op_elemwise_unary_gpu(f, a, out);
 
-        auto backward_op = [&]() {
-            if (a.grad) {
-                op_add(*a.grad, *out.grad, *a.grad);
-            }
-        };
-        out.op = std::make_shared<Op<T>>(backward_op);
+        // auto backward_op = [&]() {
+        //     if (a.grad) {
+        //         op_add(*a.grad, *out.grad, *a.grad);
+        //     }
+        // };
+        // out.op = std::make_shared<Op<T>>(backward_op);
     } else {
         assert(0); 
     }
@@ -363,15 +363,15 @@ void op_multiply(const Tensor<T> &a, const Tensor<T> &b, Tensor<T> &out)
     if (a.on_device && b.on_device && out.on_device) {
         op_elemwise_binary_w_bcast_gpu(f, a, b, out);
 
-        auto backward_op = [&]() {
-            if (a.grad) {
-                op_multiply(b, *out.grad, *a.grad);
-            }
-            if (b.grad) {
-                op_multiply(a, *out.grad, *b.grad);
-            }
-        };
-        out.op = std::make_shared<Op<T>>(backward_op);
+        // auto backward_op = [&]() {
+        //     if (a.grad) {
+        //         op_multiply(b, *out.grad, *a.grad);
+        //     }
+        //     if (b.grad) {
+        //         op_multiply(a, *out.grad, *b.grad);
+        //     }
+        // };
+        // out.op = std::make_shared<Op<T>>(backward_op);
     } else {
         assert(0); 
     }
@@ -388,12 +388,12 @@ void op_multiply(const Tensor<T> &a, T b, Tensor<T> &out)
     if (a.on_device && out.on_device) {
         op_elemwise_unary_gpu(f, a, out);
 
-        auto backward_op = [&]() {
-            if (a.grad) {
-                op_multiply(*a.grad, b, *a.grad);
-            }
-        };
-        out.op = std::make_shared<Op<T>>(backward_op);
+        // auto backward_op = [&]() {
+        //     if (a.grad) {
+        //         op_multiply(*a.grad, b, *a.grad);
+        //     }
+        // };
+        // out.op = std::make_shared<Op<T>>(backward_op);
     } else {
         assert(0); 
     }
