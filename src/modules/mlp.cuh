46d09a35fe11184eb638f451b32a6442d927c5c1
carzgil <cars.gilliland@gmail.com>
2024-05-15 15:20:19 -0400

    changing the MLP to implement stack

--- src/modules/mlp.cuh
+++ src/modules/mlp.cuh
@@ -1,52 +1,45 @@
 #pragma once
 #include "modules/linear.cuh"
 #include "ops/op_elemwise.cuh"
+#include <stack>
+#include <functional>
+
 template <typename T>
-class MLP
-{
+class MLP {
 private:
     std::vector<LinearLayer<T>> layers;
     std::vector<int> layer_dims;
     std::vector<Tensor<T>> activ;
     std::vector<Tensor<T>> d_activ;
-    std::vector<Tensor<T>> activations;
 
     int batch_size;
     int in_dim;
-    bool on_gpu;
+
+    // Stack to keep track of operations for auto differentiation
+    std::stack<std::function<void()>> back_ops;
 
 public:
     MLP(int batch_size_, int in_dim_, std::vector<int> layer_dims_, bool gpu)
-        : batch_size(batch_size_), in_dim(in_dim_), layer_dims(layer_dims_)
-    {
-        for (int i = 0; i < layer_dims.size(); i++)
-        {
-            if (i == 0)
-            {
+        : batch_size(batch_size_), in_dim(in_dim_), layer_dims(layer_dims_) {
+        for (int i = 0; i < layer_dims.size(); i++) {
+            if (i == 0) {
                 layers.emplace_back(in_dim, layer_dims[i], gpu);
-            }
-            else
-            {
+            } else {
                 layers.emplace_back(layer_dims[i - 1], layer_dims[i], gpu);
             }
         }
         // make all the activation tensors
         activ.reserve(layer_dims.size() - 1);
         d_activ.reserve(layer_dims.size() - 1);
-        for (int i = 0; i < layer_dims.size() - 1; i++)
-        {
+        for (int i = 0; i < layer_dims.size() - 1; i++) {
             activ.emplace_back(Tensor<T>(batch_size, layer_dims[i], gpu));
-            // technically, i do not need to save d_activ for backprop, but since iterative
-            // training does repeated backprops, reserving space for these tensor once is a good idea
             d_activ.emplace_back(Tensor<T>(batch_size, layer_dims[i], gpu));
         }
     }
 
-    std::vector<Parameter<T> *> parameters()
-    {
-        std::vector<Parameter<T> *> params;
-        for (int i = 0; i < layer_dims.size(); i++)
-        {
+    std::vector<Parameter<T>*> parameters() {
+        std::vector<Parameter<T>*> params;
+        for (int i = 0; i < layer_dims.size(); i++) {
             auto y = layers[i].parameters();
             params.insert(params.end(), y.begin(), y.end());
         }
@@ -59,37 +52,38 @@ public:
         }
     }
 
-    //This function peforms the forward operation of a MLP model
-    //Specifically, it should call the forward oepration of each linear layer 
-    //Except for the last layer, it should invoke Relu activation after each layer.
-    void forward(const Tensor<T> &in, Tensor<T> &out)
-    {
-        Tensor<T> current = in;
-        for (size_t i = 0; i < layers.size() - 1; ++i) {   
-            layers[i].forward(current, activations[i]);
-            op_relu(activations[i], activations[i]); 
-            current = activations[i]; 
+    void forward(const Tensor<T>& in, Tensor<T>& out) {
+        for (int i = 0; i < layers.size(); i++) {
+            if (i == 0) {
+                layers[i].forward(in, activ[i]);
+                op_relu(activ[i], activ[i]);
+                // Autodiff: Push the ReLU backward operation to the stack
+                back_ops.push([this, i]() { op_relu_back(activ[i], d_activ[i], d_activ[i]); });
+            } else if (i == layers.size() - 1) {
+                layers[i].forward(activ[i - 1], out);
+            } else {
+                layers[i].forward(activ[i - 1], activ[i]);
+                op_relu(activ[i], activ[i]);
+                // Autodiff: Push the ReLU backward operation to the stack
+                back_ops.push([this, i]() { op_relu_back(activ[i], d_activ[i], d_activ[i]); });
+            }
         }
-        layers.back().forward(current, out); 
     }
 
-    //This function perofmrs the backward operation of a MLP model.
-    //Tensor "in" is the gradients for the outputs of the last linear layer (aka d_logits from op_cross_entropy_loss)
-    //Invoke the backward function of each linear layer and Relu from the last one to the first one.
-    void backward(const Tensor<T> &d_out, const Tensor<T> &input, Tensor<T> &d_in) {
-        Tensor<T> current_gradient = d_out;
-        for (int i = layers.size() - 1; i >= 0; --i) {
-            Tensor<T> d_inputs(batch_size, (i == 0 ? in_dim : layer_dims[i-1]), on_gpu);
+    void backward(const Tensor<T>& in, const Tensor<T>& d_out, Tensor<T>& d_in) {
+        for (int i = layers.size() - 1; i >= 0; i--) {
             if (i == layers.size() - 1) {
-                layers[i].backward(current_gradient, activations[i-1], d_inputs);
-            } else if (i > 0) {
-                op_relu_back(activations[i], current_gradient, d_inputs);
-                layers[i].backward(d_inputs, activations[i-1], d_inputs);
-            } else { 
-                layers[i].backward(d_inputs, input, d_in);
+                layers[i].backward(activ[i - 1], d_out, d_activ[i - 1]);
+            } else {
+                // Autodiff: Pop the ReLU backward operation from the stack and execute it
+                back_ops.top()();
+                back_ops.pop();
+                if (i == 0) {
+                    layers[i].backward(in, d_activ[i], d_in);
+                } else {
+                    layers[i].backward(activ[i - 1], d_activ[i], d_activ[i - 1]);
+                }
             }
-            current_gradient = d_inputs; 
         }
     }
-    
 };
